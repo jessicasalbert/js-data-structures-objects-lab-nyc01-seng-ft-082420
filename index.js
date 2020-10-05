732d13644bbf233df947752ac060a5212fbfd230
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-05 02:07:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
-function deleteFromDriverByKey(driver, ey) {
+function deleteFromDriverByKey(driver, key) {
   
 }
 
